acfd24a44e7fe4d918668d428b828fea1242d9dc
HerryWEu <yihong.wu@mhp.com>
2024-11-21 19:47:16 +0800

    feat: spring security的默认配置

--- src/main/java/com/chloe/security/demo/config/WebSecurityConfig.java
+++ src/main/java/com/chloe/security/demo/config/WebSecurityConfig.java
@@ -2,10 +2,13 @@ package com.chloe.security.demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.lang.invoke.VarHandle;
 
@@ -21,6 +24,16 @@ import java.lang.invoke.VarHandle;
 @Configuration
 //@EnableWebSecurity
 public class WebSecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().authenticated()
+                )
+//                .formLogin(Customizer.withDefaults()); //默认的登录登出页
+                .httpBasic(Customizer.withDefaults());// 使用基本的授权方式，一般不用！
+        return http.build();
+    }
 /*    @Bean
     public UserDetailsService userDetailsService() {
         InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
